fd4e48bf7e0370d18be57b94eca1a29fd9815d70
Adil Hayyat <adilhayatramay@gmail.com>
2025-09-18 11:55:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ window.closePopup = closePopup;
 
 function updateUserInfo() {
   if (currentUser) {
-    userInfo.textContent = `${currentUser.email}  ||  Balance: PKR ${balance}`;
+    userInfo.textContent = `| Balance: PKR ${balance} |`;
   }
 }
 
