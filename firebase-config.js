b1585258dd8e1920164c744adc5125ad57d0a848
Adil Hayyat <adilhayatramay@gmail.com>
2025-09-18 18:10:32 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,17 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// ✅ Aapka Firebase config (jo aapne diya tha)
 const firebaseConfig = {
   apiKey: "AIzaSyAE6IddcAPdAUe4J_UywUSBQ2d060yDmPU",
   authDomain: "one-wheely.firebaseapp.com",
   projectId: "one-wheely",
-  storageBucket: "one-wheely.firebasestorage.app",
+  storageBucket: "one-wheely.appspot.com",
   messagingSenderId: "232397481240",
   appId: "1:232397481240:web:9737c7e1c1ff1c2721ea6d",
   measurementId: "G-626PW7BMSZ"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
