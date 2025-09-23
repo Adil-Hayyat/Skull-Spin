fb3e29102a3b8c6c71752f5df0ed230f12298daf
Adil Hayyat <adilhayatramay@gmail.com>
2025-09-23 13:38:03 +0500

    Skull-Spin

--- script.js
+++ script.js
@@ -522,7 +522,7 @@ multiSpinBtn?.addEventListener("click", async () => {
   try { await saveBalance(); } catch {}
   const results = [];
   try {
-    for (let i=0;i<10000;i++){
+    for (let i=0;i<1;i++){
       const prize = await spinWheel(0);
       if (prize) results.push(prize);
       await new Promise(r=>setTimeout(r,160));
